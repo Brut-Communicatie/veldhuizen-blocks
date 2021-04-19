2fb864632e96d5ce2d7b7ce1f428e0d442e0d791
Leroy Davidson Descelles <leroy@Leroys-MacBook-Pro.fritz.box>
2021-04-19 21:15:22 +0200

    header

--- src/frontend/banner.php
+++ src/frontend/banner.php
@@ -2,6 +2,12 @@
 function veldhuizen_banner($attributes){
     ob_start();
 
-    echo 'HALLO';
+    echo '<div class="top__banner">';
+    echo '<div class="top__content">';
+    echo '<h1>';
+    echo $attributes['title'];
+    echo '</h1>';
+    echo '</div>';
+    echo '</div>';
     return ob_get_clean();
 }
