ccfbbaf549966aef3c2a1d70e13d0efe4efe519e
khualu <andrespint@gmail.com>
2021-04-30 12:07:05 +0200

    Ignore this commit. This is just to start fresh. I tried a bunch of stuff to get the lightbox working as intended, but have not being able to

--- src/frontend/product-grid.php
+++ src/frontend/product-grid.php
@@ -9,18 +9,25 @@ function veldhuizen_product_grid($attributes){
 
     echo "<section class='veldhuizen__container--product-grid'>";
 
-    echo "<h1>" . $title . "</h1>";
-    echo "<div class='product-grid-wrapper'>";
-    echo "<img id='big-image' src='" . $images[0]['imgURL'] . "' />";
+     echo "<h1>" . $title . "</h1>";
+         echo "<div class='product-grid-wrapper'>";
+            echo "<img id='big-image' class='gallery-pictures' src='" . $images[0]['imgURL'] . "'onclick='openModal()' />";
 
-    foreach ($images as $image) {
-        echo "<img src='" . $image['imgURL'] . "' class='gallery-pictures' onmouseover='expandImage(this)'/>";
-    }
+            foreach ($images as $image) {
+                echo "<img src='" . $image['imgURL'] . "' class='gallery-pictures' onmouseover='expandImage(this)' onclick='openModal(event)' />";
+            }
 
-    echo "</div>";
+        echo "</div>";
+
+        echo '<div id="myModal" class="modal">';
+            echo '<span class="close cursor" onclick="closeModal()">&times;</span>';
+            echo '<a class="prev" onclick="nextSlide(-1)">&#10094;</a> <a class="next" onclick="nextSlide(1)">&#10095;</a>';
+            // echo '<img src=' . $images[0]['imgURL'] . '>';
+            echo '<img id="modal-image" src="">';
+        echo '</div>';
+
     echo "</section>";
 
-    // echo "<script type='text/Javascript' src='behavior.js'></script>";
 
     wp_enqueue_script( 'veldhuizen-product-gallery', get_template_directory_uri() . '/js/galleryBehavior.js', false, false );
 
