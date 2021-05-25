b0bea1a84e442660b6a715c0c47fbfe813fde269
Leroy Davidson Descelles <leroy@Leroys-MacBook-Pro-3.local>
2021-05-25 16:00:36 +0200

    footer links product

--- src/frontend/product-footer.php
+++ src/frontend/product-footer.php
@@ -10,10 +10,17 @@ function veldhuizen_product_footer($attributes) {
     // }
 
     ob_start();
+    global $post;
+    var_dump($post);
+    $next = get_next_post();
+    $prev = get_previous_post();
+
+    $nextLink = get_permalink( $next->ID );
+    $prevLink = get_permalink( $prev->ID );
 
     echo '<div class="veldhuizen__container--product-footer no-print">';
-    echo '<a className="footer-links">' . '<   ' . $items[rand(0, (count($items))-1)]['productTitle'] . '</a>';
-    echo '<a className="footer-links">' . $items[rand(0, (count($items))-1)]['productTitle'] . '   >'. '</a>';
+    echo '<a class="footer-links" href="'. $prevLink .'">' . '<   ' . $prev->post_title . '</a>';
+    echo '<a class="footer-links" href="'. $nextLink .'">' . $next->post_title . '   >'. '</a>';
     echo '</div>';
     
     return ob_get_clean();
