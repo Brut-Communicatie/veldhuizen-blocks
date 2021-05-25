6320344b9dba81911fd6e91735c1257e6d34aba5
khualu <andrespint@gmail.com>
2021-05-25 16:13:56 +0200

    Removed comments from product-footer.php

--- src/frontend/product-footer.php
+++ src/frontend/product-footer.php
@@ -1,14 +1,6 @@
 <?php
 
 function veldhuizen_product_footer($attributes) {
-
-    // $items = $attributes['products'][0]['productId'];
-    $items = $attributes['products'];
-
-    // function randomProduct($items) {
-    //     return rand(0, count($items));
-    // }
-
     ob_start();
     global $post;
     $next = get_next_post();
@@ -18,7 +10,7 @@ function veldhuizen_product_footer($attributes) {
     $prevLink = get_permalink( $prev->ID );
 
     echo '<div class="veldhuizen__container--product-footer no-print">';
-    echo '<a class="footer-links" href="'. $prevLink .'">' . '<   ' . "Vorige product". '</a>';           // $prev->post_title
+    echo '<a class="footer-links" href="'. $prevLink .'">' . '<   ' . "Vorige product". '</a>';              // $prev->post_title
     echo '<a class="footer-links" href="'. $nextLink .'">' . "Volgende product" . '   >'. '</a>';            // $next->post_title
     echo '</div>';
     
