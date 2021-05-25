cc7cd088379f64fa9fc5f632339c30d40df5d9ce
khualu <andrespint@gmail.com>
2021-05-25 16:08:49 +0200

    Changed previous and next product titles to simpler names

--- src/frontend/product-footer.php
+++ src/frontend/product-footer.php
@@ -18,8 +18,8 @@ function veldhuizen_product_footer($attributes) {
     $prevLink = get_permalink( $prev->ID );
 
     echo '<div class="veldhuizen__container--product-footer no-print">';
-    echo '<a class="footer-links" href="'. $prevLink .'">' . '<   ' . $prev->post_title . '</a>';
-    echo '<a class="footer-links" href="'. $nextLink .'">' . $next->post_title . '   >'. '</a>';
+    echo '<a class="footer-links" href="'. $prevLink .'">' . '<   ' . "Vorige product". '</a>';           // $prev->post_title
+    echo '<a class="footer-links" href="'. $nextLink .'">' . "Volgende product" . '   >'. '</a>';            // $next->post_title
     echo '</div>';
     
     return ob_get_clean();
