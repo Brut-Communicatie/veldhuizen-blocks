8abe561302cee06378ef40df91a69c78a259e370
Leroy Davidson Descelles <leroy@Leroys-MacBook-Pro-3.local>
2021-05-27 15:34:29 +0200

    uncomment

--- src/frontend/product-footer.php
+++ src/frontend/product-footer.php
@@ -8,31 +8,31 @@ function veldhuizen_product_footer($attributes) {
 
     ob_start();
 
-    // global $post;
+    global $post;
+   var_dump($post);
+    $parent = get_post_parent($post->ID);
    
-    // $parent = get_post_parent($post->ID);
-   
-    // $post_type = $post->post_type;
+    $post_type = $post->post_type;
 
-    // $args = array(
-    //     'posts_per_page' => -1,
-    //     'order'          => 'ASC',
-    //     'post_parent'    => $parent->ID,
-    //     'post_type'      => $post_type,
-    // );
+    $args = array(
+        'posts_per_page' => -1,
+        'order'          => 'ASC',
+        'post_parent'    => $parent->ID,
+        'post_type'      => $post_type,
+    );
 
 
-    // $siblingArray = [];
-    // $siblings = get_children($args);
+    $siblingArray = [];
+    $siblings = get_children($args);
  
-    // foreach($siblings as $sibling){
-    //     $siblingArray[] = $sibling->ID;
-    // }
+    foreach($siblings as $sibling){
+        $siblingArray[] = $sibling->ID;
+    }
  
-    // $currentKey = array_search($post->ID, $siblingArray);
+    $currentKey = array_search($post->ID, $siblingArray);
 
-    // $prevID = $siblingArray[$currentKey - 1] ? $siblingArray[$currentKey - 1] : $siblingArray[count($siblingArray) - 1];
-    // $nextID = $siblingArray[$currentKey + 1] ? $siblingArray[$currentKey + 1] : $siblingArray[0];
+    $prevID = $siblingArray[$currentKey - 1] ? $siblingArray[$currentKey - 1] : $siblingArray[count($siblingArray) - 1];
+    $nextID = $siblingArray[$currentKey + 1] ? $siblingArray[$currentKey + 1] : $siblingArray[0];
 
     
     echo '<div class="veldhuizen__container--product-footer no-print">';
