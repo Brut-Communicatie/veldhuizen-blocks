3b55fce1dc61f23c685e4bb01607f314b028b94f
Leroy Davidson Descelles <leroy@Leroys-MacBook-Pro-3.local>
2021-05-27 14:23:30 +0200

    post

--- src/frontend/product-footer.php
+++ src/frontend/product-footer.php
@@ -1,33 +1,38 @@
 <?php
 
 function veldhuizen_product_footer($attributes) {
+  
+
     global $post;
-    var_dump('test 1');
+    global $wp_query;
+    var_dump($wp_query);
+   
     $parent = get_post_parent($post->ID);
-    var_dump($post);
+   
     $post_type = $post->post_type;
-    var_dump('test 2');
-    var_dump($post_type);
+
     $args = array(
         'posts_per_page' => -1,
         'order'          => 'ASC',
         'post_parent'    => $parent->ID,
         'post_type'      => $post_type,
     );
-    var_dump('test 2');
+
 
     $siblingArray = [];
     $siblings = get_children($args);
-    var_dump('test 3');
+ 
     foreach($siblings as $sibling){
         $siblingArray[] = $sibling->ID;
     }
-    var_dump('test 4');
+ 
     $currentKey = array_search($post->ID, $siblingArray);
-    var_dump('test 5');
+
     $prevID = $siblingArray[$currentKey - 1] ? $siblingArray[$currentKey - 1] : $siblingArray[count($siblingArray) - 1];
     $nextID = $siblingArray[$currentKey + 1] ? $siblingArray[$currentKey + 1] : $siblingArray[0];
-    var_dump('test 6');
+
+
+
     ob_start();
     echo '<div class="veldhuizen__container--product-footer no-print">';
     echo '<a class="footer-links" href="'. get_the_permalink( $prevID ) .'"><  Vorige product</a>';
