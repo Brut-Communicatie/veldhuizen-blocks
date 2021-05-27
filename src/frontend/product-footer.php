9c4af70fe1f2990b980fa1953341c3166a284a5d
Leroy Davidson Descelles <leroy@Leroys-MacBook-Pro-3.local>
2021-05-27 13:33:24 +0200

    navigation

--- src/frontend/product-footer.php
+++ src/frontend/product-footer.php
@@ -3,16 +3,34 @@
 function veldhuizen_product_footer($attributes) {
     ob_start();
     global $post;
-    $next = get_next_post();
-    $prev = get_previous_post();
 
-    $nextLink = get_permalink( $next->ID );
-    $prevLink = get_permalink( $prev->ID );
+    $parent = get_post_parent($post->ID);
+    $post_type = $post->post_type;
 
+    $args = array(
+        'posts_per_page' => -1,
+        'order'          => 'ASC',
+        'post_parent'    => $parent->ID,
+        'post_status'    => null,
+        'post_type'      => $post_type,
+    );
+
+    $siblingArray = [];
+    $siblings = get_children($args);
+
+    foreach($siblings as $sibling){
+        $siblingArray[] = $sibling->ID;
+    }
+
+    $currentKey = array_search($post->ID, $siblingArray);
+
+    $prevID = $siblingArray[$currentKey - 1] ? $siblingArray[$currentKey - 1] : $siblingArray[count($siblingArray) - 1];
+    $nextID = $siblingArray[$currentKey + 1] ? $siblingArray[$currentKey + 1] : $siblingArray[0];
+    
     echo '<div class="veldhuizen__container--product-footer no-print">';
-    echo '<a class="footer-links" href="'. $prevLink .'">' . '<   ' . "Vorige product". '</a>';              // $prev->post_title
-    echo '<a class="footer-links" href="'. $nextLink .'">' . "Volgende product" . '   >'. '</a>';            // $next->post_title
-    echo '</div>';
+
+    echo '<a class="footer-links" href="'. get_the_permalink( $prevID ) .'">' . '<   ' . "Vorige product". '</a>';
+    echo '<a class="footer-links" href="'. get_the_permalink( $nextID ) .'">' . "Volgende product" . '   >'. '</a>';
     
     return ob_get_clean();
 }
